4dbb7b489fe693c932bc40964df9cdcfd942b5e5
John Crepezzi <john.crepezzi@gmail.com>
2013-12-23 19:42:13 -0500

    module

---
+++ test/examples/settings_spec.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+var settings = require('../../lib/settings');
+
+describe(settings, function () {
+
+  describe('no default', function () {
+
+    before(function () {
+      this.o = settings.create();
+    });
+
+    it('should have undefined value by default', function () {
+      assert.strictEqual(this.o.h, undefined);
+    });
+
+    it('should be able to set a value', function () {
+      var value = 5;
+      this.o.h = value;
+      assert.strictEqual(this.o.h, value);
+    });
+
+  });
+
+  describe('with a default', function () {
+
+    var defaultValue = 1;
+
+    before(function () {
+      this.o = settings.create();
+      this.o.setDefault('h', defaultValue);
+    });
+
+    it('should get the default by default', function () {
+      assert.strictEqual(this.o.h, defaultValue);
+    });
+
+    it('should be able to override the default', function () {
+      this.o.h = null;
+      assert.strictEqual(this.o.h, null);
+    });
+
+    it('should be able to rever to default', function () {
+      this.o.h = 123; // something else
+      this.o.h = undefined;
+      assert.strictEqual(this.o.h, defaultValue);
+    });
+
+  });
+
+});
